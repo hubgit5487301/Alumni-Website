13f5165679f7fc4d1df0d941256a58fad0cf8d9f
hubgit5487301 <vikasyy5487301@gmail.com>
2024-11-30 19:16:11 +0530

    alumni search backend completed

--- config/routes/userroutes.js
+++ config/routes/userroutes.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const path = require('path');
-const crypto = require('crypto');
 
 const router = express();
 const user = require('../../models/alumni.js');
-const {hashPassword, verifypassword} = require('../util.js');
 
 
 
@@ -62,6 +59,31 @@ router.get(`/users/:userid`, async (req,res) =>{
   }
 })
 
+router.get('/alumni-search', async (req, res) => {
+  const input = req.query.name;
+  try{
+    const results = await user.find({
+      personname: { $regex: input, $options: 'i' }
+    });
+    results.forEach((user, index) => {
+      const user_search = user.toObject();
+      user_search._id = null;
+      user_search.salt = null;
+      user_search.passwordhash = null;
+      user_search.details = null;
+      user_search.email = null;
+      results[index] = user_search;
+    });
+  if(results.length === 0){
+    return res.status(200).json([]);
+  }
+  res.status(200).json(results);
+  }
+  catch(err) {
+    res.status(500).json({error: 'internal servor error'});
+  }
+});
+
 
 router.get('/event-directory', (req, res) => {
   res.sendFile(path.join(__dirname, '..', '..', 'protected', 'event-directory.html'))
@@ -75,5 +97,7 @@ router.get('/job-directory', (req, res) => {
 router.get('/contact-us', (req, res) => {
   res.sendFile(path.join(__dirname, '..', '..', 'protected', 'contact-us.html'))
 })
+
+
 
 module.exports = router;
--- protected/protected-scripts/alumni-directory.js
+++ protected/protected-scripts/alumni-directory.js
@@ -26,3 +26,39 @@ document.querySelectorAll('.js-person').forEach((user, index) => {
       });
     });
   });});
+
+const searchInput = document.querySelector('.js-search-input');
+const result = document.querySelector('.js-search-output');
+
+let searchHtml = '';
+searchInput.addEventListener('input', () =>{
+  const input = document.querySelector('.js-search-input').value.trim();
+  if(input.trim() === '') {
+    result.innerHTML = '';
+  }
+  fetch(`http://localhost:8000/protected/alumni-search?name=${encodeURIComponent(input)}`)
+  .then(response => {
+    if(!response.ok) {
+      throw new Error('response not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    searchHtml = '';
+    data.forEach(user =>
+      searchHtml +=`
+        <div class="person js-person">
+            <img class="person-image" src="${user.personimage}">
+            <div class="person-name">
+              <h>${user.personname}</h>
+            </div>
+        </div>
+`     )
+      result.innerHTML = searchHtml;
+})
+  .catch(err=> {
+    console.error('error fetching data',err);
+    result.innerHTML = '<div>There was an error fetching the data</div>';
+  })
+ 
+})
