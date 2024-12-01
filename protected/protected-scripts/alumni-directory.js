095a7e8552e680eccc3dbb5f117474a4573a4036
hubgit5487301 <vikasyy5487301@gmail.com>
2024-12-01 10:30:09 +0530

    completed search feature for alumni lookup,both frontend and backend

--- protected/protected-scripts/alumni-directory.js
+++ protected/protected-scripts/alumni-directory.js
@@ -33,10 +33,18 @@ const result = document.querySelector('.js-search-output');
 let searchHtml = '';
 searchInput.addEventListener('input', () =>{
   const input = document.querySelector('.js-search-input').value.trim();
-  if(input.trim() === '') {
-    result.innerHTML = '';
-  }
-  fetch(`http://localhost:8000/protected/alumni-search?name=${encodeURIComponent(input)}`)
+  let debounce = '';
+  clearTimeout(debounce);
+
+  debounce = setTimeout(() => {
+    if(input.trim() === '') {
+      result.innerHTML = '';
+      result.style.display = 'none';
+    }
+    else {
+      result.style.display = 'grid';
+    }
+    fetch(`http://localhost:8000/protected/alumni-search?name=${encodeURIComponent(input)}`)
   .then(response => {
     if(!response.ok) {
       throw new Error('response not ok');
@@ -45,7 +53,12 @@ searchInput.addEventListener('input', () =>{
   })
   .then(data => {
     searchHtml = '';
-    data.forEach(user =>
+    if(data.length === 0) {
+        result.classList.add('show');
+        result.innerHTML = `<div class="text js-text">No user with name "${input}" found</div> `;
+        document.querySelector('.js-text').classList.add('show');
+     }
+    data.forEach(user =>{
       searchHtml +=`
         <div class="person js-person">
             <img class="person-image" src="${user.personimage}">
@@ -53,12 +66,25 @@ searchInput.addEventListener('input', () =>{
               <h>${user.personname}</h>
             </div>
         </div>
-`     )
-      result.innerHTML = searchHtml;
+`    
+       result.innerHTML = searchHtml;
+      setTimeout(() => {
+        document.querySelectorAll('.js-person').forEach(person => {
+          person.classList.add('show');
+        });
+      }, 10);
+      result.classList.add('show');
+      document.querySelectorAll('.js-person').forEach((user, index) => {
+        user.addEventListener('click' ,() => {
+          const userid = data[index].userid;
+          window.location.href = `profile.html?userid=${userid}`;
+        });
+      });
+    })
 })
   .catch(err=> {
     console.error('error fetching data',err);
-    result.innerHTML = '<div>There was an error fetching the data</div>';
+    result.innerHTML = '<div>There was an error fetching the data please reload the webpage</div>';
   })
  
-})
+}, 250)})
