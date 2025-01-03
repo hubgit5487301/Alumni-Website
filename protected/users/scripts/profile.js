bb17325a8a9c6602a290578c78da5946b985a18b
hubgit5487301 <vikasyy5487301@gmail.com>
2025-01-03 12:14:07 +0530

    add loading indicator to contact page and enhance profile styles for better user experience

--- protected/users/scripts/profile.js
+++ protected/users/scripts/profile.js
@@ -4,14 +4,10 @@ const urlParams = new URLSearchParams(window.location.search);
 const userid = urlParams.get('userid');
 const data = await getdata(`/users/${userid}`);
 
-  
 document.querySelector('.profile-pic').src = data.personimage;
 document.querySelector('.user-name').textContent = data.personname;
-
 document.querySelector('.batch').textContent = `Batch: ${data.details.batch}`;
 document.querySelector('.branch').textContent = `Branch: ${data.details.branch}`;
-
-
 document.querySelector('.about-me').innerText = data.details.aboutme;
 document.querySelector('.education').innerText = data.details.education;
 document.querySelector('.role').innerText = data.details.currentrole;
